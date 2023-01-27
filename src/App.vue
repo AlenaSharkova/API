336441776007ad7e58ff844e3692b0e3f5a1aab6
AlenaSharkova <alena.s_00@mail.ru>
2023-01-27 11:41:23 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <EpisodeList />
+  </div>
+</template>
+
+<script>
+import EpisodeList from '@/components/EpisodeList';
+export default {
+  name: 'App',
+  components: {
+    EpisodeList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EpisodeList.vue
@@ -0,0 +1,69 @@
+<template>
+    <h1>Episode List</h1>
+    <div class="episode-list">
+        <div class="episode" v-for="item in episodes" :key="item.id">
+          <h2 class="episode__name">{{ item.name }}</h2>
+          <p class="episode__air-date">{{item.air_date }}</p>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+console.log(axios.isCancel('something'));
+export default {
+  data() {
+    return {
+      episodes: null
+    };
+  },
+  methods: {
+    getEpisodes: async function() {
+      await axios
+        .get('https://rickandmortyapi.com/api/episode')
+        .then(response => (this.episodes = (response.data.results)))
+        .catch(error => {
+          console.log(error)
+        }); 
+    }
+  },
+  mounted() {
+    this.getEpisodes();
+  }
+
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.episode__name {
+  font-size: 16px;
+}
+
+.episode__air-date {
+  font-size: 14px;
+}
+
+.episode-list {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.episode {
+  width: 150px;
+  height: 150px;
+  margin-bottom: 20px;
+  margin-right: 20px;
+  padding: 20px;
+  background-color: antiquewhite;
+  border-radius: 10%;
+  box-shadow: 20px 5px 14px #676767;
+}
+
+</style>
